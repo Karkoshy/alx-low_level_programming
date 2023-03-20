1d327546847e9e9890219b25633d0c13aa858cf6
karkoshy <kareemkimo1303@gmail.com>
2023-03-20 15:39:27 -0700

    k

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+  *main - Entry point
+  *Return: Always 0
+  */
+int main(void)
+{
+int n = 97;
+	for (n = 97; n < 123; n++)
+	{
+		putchar(n);
+	}
+putchar('\n');
+	return (0);
+}
